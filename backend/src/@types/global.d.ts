f3fe425ce94727ead97671bdba69840c2394cc94
Naohito-T <naohito.tanaka0523@gmail.com>
2022-10-09 16:32:23 +0900

    :test_tube: [add] create frontend with cors finish

--- backend/src/@types/global.d.ts
+++ backend/src/@types/global.d.ts
@@ -3,6 +3,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly CORS_ALLOW_ORIGIN: string;
     readonly TYPEORM_HOST: string;
     readonly TYPEORM_PORT: string;
     readonly TYPEORM_USER: string;
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  pageExtensions: ['page.tsx', 'page.ts'],
+  compiler: {
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
